6d50f75ba42539ec0e76766b87e100da5b60d06a
483933 <483933@LT085767.cts.com>
2019-03-17 16:28:56 +0530

    git jenkins

---
+++ MavenFinal_One/src/test/java/Basic/MavenGit.java
@@ -0,0 +1,27 @@
+package Basic;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class MavenGit {
+	
+	WebDriver driver;
+	@Test
+	public void launchchrome () throws Exception
+	{
+		
+		WebDriverManager.chromedriver().version("2.41").setup();
+		driver = new ChromeDriver();
+		driver.get("http://www.google.com");
+		Thread.sleep(5000);
+		System.out.println("Success");
+		driver.quit();
+		
+		
+		
+	}
+
+}
